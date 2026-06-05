68a9f3bf4c3b97415f15b3344bfc8051da1cb2b2
Kira-Window <duycoder544@gmail.com>
2026-06-05 23:35:47 +0700

    refactor: update package.json to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // Dự án của bạn đang dùng tailwind v4
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+  
+  preview: {
+    allowedHosts: true
+  },
+  server: {
+    allowedHosts: true 
+  }
 })
