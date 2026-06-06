c13aec690b595a87e6a304b1dc39e97ffa52bccc
Kira-Window <duycoder544@gmail.com>
2026-06-06 21:20:32 +0700

    impleament login with google adn refactore state loging

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App.jsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <App /> 
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
